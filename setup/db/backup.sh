a98707fefdaf67c2612779b89a45a8a0d5e653d5
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-05-02 16:01:34 +0200

    Add backup script

---
+++ setup/db/backup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# AWS Policy JSON
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "VisualEditor0",
+#             "Effect": "Allow",
+#             "Action": [
+#                 "s3:PutObject",
+#                 "s3:ListBucket"
+#             ],
+#             "Resource": [
+#                 "arn:aws:s3:::nl-db-backup/*",
+#                 "arn:aws:s3:::nl-db-backup"
+#             ]
+#         }
+#     ]
+# }
+
+# crontab -l > mycron
+# echo "0 3 * * * ~/db/backup.sh" >> mycron
+# crontab mycron
+# rm mycron
+
+DB_USER="root"
+DB_PASSWORD="changemeplease"
+DB_HOST="mynl.pl"
+DB_PORT="3307"
+S3_BUCKET="nl-db-backup"
+S3_STORAGE_CLASS="ONEZONE_IA"
+AWS_ACCESS_KEY="AAAAAAAA"
+AWS_SECRET_KEY="XXXXX"
+AWS_REGION="eu-central-1"
+
+# List your database names separated by spaces
+DATABASES="cod2_zom nl"
+
+# Create a temporary backup directory
+BACKUP_DIR=$(mktemp -d)
+echo "Created temp backup dir: ${BACKUP_DIR}"
+
+# Clean up the temporary backup directory
+cleanup() {
+    rm -rf "${BACKUP_DIR}"
+    echo "Removed temp backup dir: ${BACKUP_DIR}"
+}
+
+# Register the cleanup function to be called on exit
+trap cleanup EXIT
+
+for DB_NAME in $DATABASES
+do
+    SQL_FILE="backup_${DB_NAME}.sql"
+    echo "Dumping database ${DB_NAME}..."
+
+    mysqldump --skip-column-statistics -h ${DB_HOST} -P ${DB_PORT} -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} > ${BACKUP_DIR}/${SQL_FILE}
+    echo " - Dumped database ${DB_NAME} to ${SQL_FILE}"
+
+    gzip ${BACKUP_DIR}/${SQL_FILE}
+    echo " - Packed ${SQL_FILE}"
+
+    S3_PATH=s3://${S3_BUCKET}/${SQL_FILE}.gz
+    AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY} AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY} aws s3 cp ${BACKUP_DIR}/${SQL_FILE}.gz ${S3_PATH} --region ${AWS_REGION} --storage-class ${S3_STORAGE_CLASS}
+    echo "- Sent backup to $S3_PATH in ${AWS_REGION}"
+
+    echo "Dumping database ${DB_NAME}... done"
+done
